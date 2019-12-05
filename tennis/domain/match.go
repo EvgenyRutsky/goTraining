86bbb28f27445566b44b1fcde360e693f8a3b09a
EvgenyRutsky <e.rutskiy97@gmail.com>
2019-12-05 22:23:46 +0300

    finished channel communication logic

--- tennis/domain/match.go
+++ tennis/domain/match.go
@@ -1,5 +1,20 @@
 package domain
 
+import "sync"
+
 type Match struct {
 
+}
+
+func (m Match) Start(p1 *Player, p2 *Player) {
+	wg := &sync.WaitGroup{}
+	c := make(chan string)
+
+	wg.Add(1)
+	go p1.Play(c, wg)
+	c<-"ball"
+	wg.Add(1)
+	go p2.Play(c, wg)
+
+	wg.Wait()
 }
